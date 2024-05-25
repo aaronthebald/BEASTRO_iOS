67a898840fc4c7a6f8ce919273d720e684e4fa53
Aaron Wilson <124102001+aaronthebald@users.noreply.github.com>
2024-05-25 19:47:33 -0400

    Initial Commit

---
+++ BEASTRO_iOS/BEASTRO_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  BEASTRO_iOSApp.swift
+//  BEASTRO_iOS
+//
+//  Created by Aaron Wilson on 5/25/24.
+//
+
+import SwiftUI
+
+@main
+struct BEASTRO_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
