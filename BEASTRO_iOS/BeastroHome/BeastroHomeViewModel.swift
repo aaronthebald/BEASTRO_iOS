8c4e07e7efa08f4b3bc3808d4b9872f01806bc59
Aaron Wilson <124102001+aaronthebald@users.noreply.github.com>
2024-05-28 12:23:15 -0400

    Simplifying the logic

--- BEASTRO_iOS/BeastroHome/BeastroHomeViewModel.swift
+++ BEASTRO_iOS/BeastroHome/BeastroHomeViewModel.swift
@@ -33,7 +33,7 @@ class BeastroHomeViewModel: ObservableObject {
     let dateAndTimeService = DateAndTimeService()
     let currentDayFormatter = DateFormatter()
     let timeFormatter = DateFormatter()
-    let timeReadableinputFormatter = DateFormatter()
+    let timeReadableInputFormatter = DateFormatter()
 
     
     var networkingService: NetworkingServiceProtocol
@@ -197,78 +197,79 @@ class BeastroHomeViewModel: ObservableObject {
         
         formattedDaysTimes = newArray
         print(formattedDaysTimes)
-        getOpenStatus()
-    }
-    
-    
-    func getOpenStatus() {
-        // Get the current date
-        let currentDate = Date()
-        let calendar = Calendar.current
-        // "EEEE" gives the full name of the day of the week
-        currentDayFormatter.dateFormat = "EEEE"
-        timeFormatter.dateFormat = "HH:mm:ss"
-        // Get the day of the week string
-        let dayOfWeek = currentDayFormatter.string(from: currentDate)
-        currentDay = dayOfWeek
-        guard let todaysHoursObject = formattedDaysTimes.first(where: {$0.weekday == currentDay}) else {
-            print("The Full function isn't running")
-            return
-        }
-        //        Restaurant is closed
-        if todaysHoursObject.startTimes == [] && todaysHoursObject .startTimes == [] {
-            let currentTimeString = timeFormatter.string(from: currentDate)
-            if let currentTime = timeFormatter.date(from: currentTimeString) {
-                restaurantIsOpen = false
-                openStatusLight = .red
-                guard let nextDayOpen = getNextOpenDay(businessHours: formattedDaysTimes) else {
-                    print("Part A Is broken")
-                    return
-                }
-                guard let nextOpenDateString = nextDayOpen.startTimes.first else {
-                    print("Part B is broken")
-                    return
-                }
-                if let nextDayOpenDate = currentDayFormatter.date(from: nextDayOpen.weekday),
-                   let nextDayOpenTime = timeFormatter.date(from: nextOpenDateString) {
-                    let within24Hours = calendar.date(byAdding: .hour, value: -24, to: nextDayOpenTime)!
-                    let isOpeningWithin24Hours = currentTime >= nextDayOpenTime && currentTime >= within24Hours
-                    if isOpeningWithin24Hours {
-                        openStatusText = "Opens again at \(makeTimeReadable(input: nextOpenDateString))"
-                    } else {
-                        openStatusText = "Opens \(nextDayOpen.weekday) at \(makeTimeReadable(input: nextOpenDateString))"
-                    }
-                }
-            }
-        }
+        mainTextController()
         
-        let currentTimeString = timeFormatter.string(from: currentDate)
-        for openTime in todaysHoursObject.startTimes {
-            for closedTime in todaysHoursObject.endTimes {
-                if let openDate = timeFormatter.date(from: openTime),
-                   let closeDate = timeFormatter.date(from: closedTime),
-                   let currentTime = timeFormatter.date(from: currentTimeString) {
-                    // Check if the Restaurant is open
-                    if currentTime >= openDate && currentTime <= closeDate {
-                        restaurantIsOpen = true
-                        openStatusLight = .green
-                        //                        Checks if Restaurant is closing within the hour
-                        let closeDateString = closeDate.description
-                        openStatusText = "Open until \(makeTimeReadable(input: closeDateString))"
-                        let oneHourBeforeClose = calendar.date(byAdding: .hour, value: -1, to: closeDate)!
-                        let isClosingSoon = currentTime >= oneHourBeforeClose && currentTime <= closeDate
-                        closingSoon = isClosingSoon
-                        if closingSoon {
-                            guard let nextDayOpen = getNextOpenDay(businessHours: formattedDaysTimes) else { return }
-                            
-                            openStatusText = "Open until \(makeTimeReadable(input: closeDateString)), reopens at THIS NEEDS TO BE FIXED"
-                            openStatusLight = .yellow
-                        }
-                    }
-                }
-            }
-        }
     }
+    
+    
+//    func getOpenStatus() {
+//        // Get the current date
+//        let currentDate = Date()
+//        let calendar = Calendar.current
+//        // "EEEE" gives the full name of the day of the week
+//        currentDayFormatter.dateFormat = "EEEE"
+//        timeFormatter.dateFormat = "HH:mm:ss"
+//        // Get the day of the week string
+//        let dayOfWeek = currentDayFormatter.string(from: currentDate)
+//        currentDay = dayOfWeek
+//        guard let todaysHoursObject = formattedDaysTimes.first(where: {$0.weekday == currentDay}) else {
+//            print("The Full function isn't running")
+//            return
+//        }
+//        //        Restaurant is closed
+//        if todaysHoursObject.startTimes == [] && todaysHoursObject .startTimes == [] {
+//            let currentTimeString = timeFormatter.string(from: currentDate)
+//            if let currentTime = timeFormatter.date(from: currentTimeString) {
+//                restaurantIsOpen = false
+//                openStatusLight = .red
+//                guard let nextDayOpen = getNextOpenDay(businessHours: formattedDaysTimes) else {
+//                    print("Part A Is broken")
+//                    return
+//                }
+//                guard let nextOpenDateString = nextDayOpen.startTimes.first else {
+//                    print("Part B is broken")
+//                    return
+//                }
+//                if let nextDayOpenDate = currentDayFormatter.date(from: nextDayOpen.weekday),
+//                   let nextDayOpenTime = timeFormatter.date(from: nextOpenDateString) {
+//                    let within24Hours = calendar.date(byAdding: .hour, value: -24, to: nextDayOpenTime)!
+//                    let isOpeningWithin24Hours = currentTime >= nextDayOpenTime && currentTime >= within24Hours
+//                    if isOpeningWithin24Hours {
+//                        openStatusText = "Opens again at \(makeTimeReadable(input: nextOpenDateString))"
+//                    } else {
+//                        openStatusText = "Opens \(nextDayOpen.weekday) at \(makeTimeReadable(input: nextOpenDateString))"
+//                    }
+//                }
+//            }
+//        }
+//        
+//        let currentTimeString = timeFormatter.string(from: currentDate)
+//        for openTime in todaysHoursObject.startTimes {
+//            for closedTime in todaysHoursObject.endTimes {
+//                if let openDate = timeFormatter.date(from: openTime),
+//                   let closeDate = timeFormatter.date(from: closedTime),
+//                   let currentTime = timeFormatter.date(from: currentTimeString) {
+//                    // Check if the Restaurant is open
+//                    if currentTime >= openDate && currentTime <= closeDate {
+//                        restaurantIsOpen = true
+//                        openStatusLight = .green
+//                        //                        Checks if Restaurant is closing within the hour
+//                        let closeDateString = closeDate.description
+//                        openStatusText = "Open until \(makeTimeReadable(input: closeDateString))"
+//                        let oneHourBeforeClose = calendar.date(byAdding: .hour, value: -1, to: closeDate)!
+//                        let isClosingSoon = currentTime >= oneHourBeforeClose && currentTime <= closeDate
+//                        closingSoon = isClosingSoon
+//                        if closingSoon {
+//                            guard let nextDayOpen = getNextOpenDay(businessHours: formattedDaysTimes) else { return }
+//                            
+//                            openStatusText = "Open until \(makeTimeReadable(input: closeDateString)), reopens at THIS NEEDS TO BE FIXED"
+//                            openStatusLight = .yellow
+//                        }
+//                    }
+//                }
+//            }
+//        }
+//    }
     func getCurrentDayString() -> String {
         let now = Date()
         let dateFormatter = DateFormatter()
@@ -319,12 +320,12 @@ class BeastroHomeViewModel: ObservableObject {
     
     func makeTimeReadable(input: String) -> String {
         var returnedString = ""
-        timeReadableinputFormatter.dateFormat = "HH:mm:ss"
+        timeReadableInputFormatter.dateFormat = "HH:mm:ss"
         
         // Convert the input string to a Date object
         if input == "24:00:00" {
             let newInput = "00:00:00"
-            if let date = timeReadableinputFormatter.date(from: newInput) {
+            if let date = timeReadableInputFormatter.date(from: newInput) {
                 // Create a DateFormatter for the output format
                 let outputFormatter = DateFormatter()
                 outputFormatter.dateFormat = "h a"
@@ -338,7 +339,7 @@ class BeastroHomeViewModel: ObservableObject {
                 print("Invalid input time format")
             }
         } else {
-            if let date = timeReadableinputFormatter.date(from: input) {
+            if let date = timeReadableInputFormatter.date(from: input) {
                 // Create a DateFormatter for the output format
                 let outputFormatter = DateFormatter()
                 outputFormatter.dateFormat = "h a"
