9a01e9ccc6bead76d28ed3e874513ffb2a334da1
Aaron Wilson <124102001+aaronthebald@users.noreply.github.com>
2024-05-30 16:04:32 -0400

    Cleaned up the fetchBusinessHours func in NetworkingService

--- BEASTRO_iOS/ServicesAndModels/NetworkingService.swift
+++ BEASTRO_iOS/ServicesAndModels/NetworkingService.swift
@@ -22,23 +22,20 @@ class NetworkingService: NetworkingServiceProtocol {
     }
     
     func fetchBusinessHours() async throws -> [Hour] {
-        var retunedArray: [Hour] = []
         guard let url = URL(string: urlString) else {
             let error = URLError.invalidURL
             throw error
         }
         do {
             let (data, response) = try await URLSession.shared.data(from: url)
-//            TODO: Come back and include edge cases
             guard (response as? HTTPURLResponse)?.statusCode == 200 else {
                 throw URLError.badResponse
             }
-            let decodedData = try jsonDecoder.decode(HoursResponse.self, from: data)
-            retunedArray = decodedData.hours
+            let hoursResponse = try jsonDecoder.decode(HoursResponse.self, from: data)
+            return hoursResponse.hours
         } catch {
             throw error
         }
-        return retunedArray
     }
 }
 
